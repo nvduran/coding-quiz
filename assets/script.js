828c494f3f72d8a46d2626b1ffafe279fea60235
Nick Duran <nvduran@gmail.com>
2021-01-10 21:53:35 -0600

    logic outline

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,10 @@
 var timerEl = document.querySelector("#timer");
 var secondsVal = 0;
+var answer1El = document.querySelector("#answer1");
+var answer2El = document.querySelector("#answer2");
+var answer3El = document.querySelector("#answer3");
+var answer4El = document.querySelector("#answer4");
+
 
 var timeInterval = setInterval(function(){
      secondsVal++
@@ -11,3 +16,4 @@ var timeInterval = setInterval(function(){
      }
 },1000);
 
+answer1El.textContent="hello"; 
