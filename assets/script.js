1dfabf7c1a9b059b79c84ef8a953ff56bc14ec83
Nick Duran <nvduran@gmail.com>
2021-01-10 21:35:56 -0600

    timer seconds fix

--- assets/script.js
+++ assets/script.js
@@ -3,12 +3,11 @@ var secondsVal = 0;
 
 var timeInterval = setInterval(function(){
      secondsVal++
-     var secondsRemaining = 10-secondsVal;
+     var secondsRemaining = 60-secondsVal;
      if(secondsRemaining >= 1){
           timerEl.textContent=secondsRemaining;
      }else{
           clearInterval();
      }
-          
 },1000);
 
